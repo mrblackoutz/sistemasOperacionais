f53cdfa073907891cb1da482fc515bd396053233
mrblackoutz <107147086+mrblackoutz@users.noreply.github.com>
2024-09-01 17:08:19 -0300

    Update ex02.c

--- ex02.c
+++ ex02.c
@@ -1,9 +1,11 @@
-# include <stdio.h>
-# include <locale.h>
-# include <stdlib.h>
-# include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <locale.h> 
+#include <stdlib.h> 
+#include <string.h>
 
-# define BUFFER 128
+#define BUFFER 128
 
 int main() {
     const char* nomeArquivo = "aula02.txt";
@@ -11,27 +13,31 @@ int main() {
 
     setlocale(LC_ALL, "Portuguese");
 
-    FILE *arq = fopen(nomeArquivo, "r");
-
-    if (arq == NULL) {
+    int fd = open(nomeArquivo, O_RDONLY);
+    if (fd == -1) {
         printf("Erro na abertura do arquivo.\n");
         return -1;
     }
 
     int count = 0;
+    int bytesRead;
 
-    while (fgets(buffer, sizeof(buffer), arq) != NULL) {
-        for (int i = 0; i < strlen(buffer); i++) {
+    while ((bytesRead = read(fd, buffer, sizeof(buffer) - 1)) > 0) {
+        buffer[bytesRead] = '\0';
+        for (int i = 0; i < bytesRead; i++) {
             if (buffer[i] == '\n') {
                 count++;
             }
         }
     }
-    
-    if (strlen(buffer) > 0 && buffer[strlen(buffer) - 1] != '\n') {
+
+    if (bytesRead == 0 && buffer[strlen(buffer) - 1] != '\n') {
         count++;
     }
 
     printf("\nNúmero de linhas do arquivo: %d\n\n", count);
+
+    close(fd);
+
     return 0;
 }
